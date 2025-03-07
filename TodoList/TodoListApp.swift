227c07ab9d0489a5f481a54ddb97533dd69edb43
ZinThantWin <zinthantwin1997@gmail.com>
2025-03-07 16:11:40 +0630

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Kinzo on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
